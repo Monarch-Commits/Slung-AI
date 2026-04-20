bd2950f933e84a997d375b0d8371aa4d11ca4e5e
Monarch <158782129+Monarch58272212@users.noreply.github.com>
2026-04-20 21:50:17 +0800

    test

--- app/SignInPage/page.tsx
+++ app/SignInPage/page.tsx
@@ -11,7 +11,7 @@ export default function Page() {
             {/* Header Section */}
             <div className="mx-auto flex max-w-lg flex-col gap-6">
               <div className="text-center">
-                <h1 className="mb-2 font-[Inter] text-[32px] font-bold tracking-tight">
+                <h1 className="text-center font-[Inter] text-[32px] leading-[1.5] font-semibold tracking-[-2.05px] text-[#1e293b] md:text-[48px]">
                   Welcome back
                 </h1>
                 <p className="font-[Syne] text-sm text-gray-500">
