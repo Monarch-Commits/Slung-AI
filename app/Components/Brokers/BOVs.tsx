f973fb9c263845875a023b8c691bc18c29127afa
Monarch <158782129+Monarch58272212@users.noreply.github.com>
2026-04-25 08:27:39 +0800

    ...

--- app/Components/Brokers/BOVs.tsx
+++ app/Components/Brokers/BOVs.tsx
@@ -3,13 +3,14 @@ import Image from 'next/image';
 export default function BOVs() {
   return (
     <div className="relative flex h-full w-full items-end overflow-hidden rounded-t-md bg-gradient-to-r from-[#2F90EA] via-[#84C3F9] to-[#E3EEF9]">
-      <div className="relative mx-auto aspect-[19/8] w-[95%] max-w-[700px] overflow-hidden rounded-t-lg bg-white/50 shadow-sm">
+      <div className="relative mx-auto aspect-[19/8] w-[95%] max-w-[700px] overflow-hidden rounded-t-lg">
         <Image
           src="/Brokers/Bovs.svg"
           alt="Underwriting Model"
           fill
-          className="object-cover object-top"
+          className="object-contain object-top"
           priority
+          unoptimized
           quality={100}
         />
       </div>
